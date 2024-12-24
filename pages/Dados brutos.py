3452bdfc63764e4222b0f3a6fb704df6f03612d2
Vagner Souza <66963813+vagnersouza23@users.noreply.github.com>
2024-12-24 06:20:33 -0300

    fim-cap-03

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import requests
 import pandas as pd
+from time import sleep
+
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index = False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!', icon="✅")
+    sleep(5)
+    sucesso.empty()
 
 st.title('DADOS BRUTOS')
 
@@ -37,4 +47,36 @@ with st.sidebar.expander('Quantidade de parcelas'):
     qtd_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 24, (1,24))
 
 
-st.dataframe(dados)
+query = '''
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= Preço <= @preco[1] and \
+@frete[0] <= Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0]<= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
+'''
+
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+st.dataframe(dados_filtrados)
+
+st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas.')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility='collapsed', value = 'dados')
+    nome_arquivo += '.csv'
+
+with coluna2:
+    st.download_button('Fazer o download da tabela em CSV', data=converte_csv(dados_filtrados), file_name=nome_arquivo, mime='text/csv', on_click=mensagem_sucesso)
+
+
+
+
+
+
